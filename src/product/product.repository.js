c807c8cbb90df9d1b2c0047b2fd184480017ea8e
Dominic <dominic.chan@complyadvantage.com>
2021-11-16 11:49:46 +0000

    fulfil discount feature

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,7 +4,7 @@ class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", 30.0, 22)],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", 30.0, 22, "black-friday")],
             ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", 30.0, 22)],
             ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", 30.0, 22)],
         ]);
