4dd68e64f13d00d481aeb80376d151fcc55f71ef
Dominic <dominic.chan@complyadvantage.com>
2021-11-16 12:32:38 +0000

    implement discount feature

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -43,17 +43,17 @@ describe("Pact Verification", () => {
     const stateHandlers = {
       "products exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1",  30.0, 22)]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1",  30.0, 22, "black-friday")]
         ]);
       },
       "products exist": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1",  30.0, 22)]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1",  30.0, 22, "black-friday")]
         ]);
       },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1",  30.0, 22)]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1",  30.0, 22, "black-friday")]
         ]);
       },
       "a product with ID 11 does not exist": () => {
